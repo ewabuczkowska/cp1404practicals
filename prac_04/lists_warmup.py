def407679e9259ef785f5b51f107964e286d8dea
ewabuczkowska <ewakarolina.buczkowska@my.jcu.edu.au>
2024-10-10 14:11:05 +1000

    Complete list warm up

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,26 @@
+"""
+CP1404 - Practical 4 - 1. Lists Warm Up
+"""
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# Values of the expressions:
+# 3
+# 2
+# 1
+# [3, 1, 4, 1, 5, 9]
+# [1]
+# True
+# False
+# False
+# [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# 1.
+numbers[0] = "ten"
+print(numbers)
+
+# 2.
+numbers[-1] = 1
+print(numbers)
+
+# 3.
+numbers.s
