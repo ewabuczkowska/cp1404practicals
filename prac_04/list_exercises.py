c046f65cd76328176ca4c1050803269db0d94c3e
ewabuczkowska <ewakarolina.buczkowska@my.jcu.edu.au>
2024-10-13 20:42:28 +1000

    Display list comprehensions

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -24,9 +24,9 @@ print(f"The average of the numbers is {average_number:.1f}")
 # Woefully inadequate security checker
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
-input_username = input("Please enter your username: ")
+username = input("Please enter your username: ")
 
-if input_username in usernames:
+if username in usernames:
     print("Access granted")
 else:
     print("Access denied")
