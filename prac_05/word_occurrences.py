eaa83f57a00df8c38145f8548a8a348858e957c4
ewabuczkowska <ewakarolina.buczkowska@my.jcu.edu.au>
2024-10-19 20:45:50 +1000

    Update prac 5 word occurrences exercise

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,26 @@
+"""
+CP1404 - Practical 5 - 3. Word Occurrences
+Estimate: 30 minutes
+Actual:   minutes
+"""
+
+def main():
+    user_input = input("Text: ")
+    word_to_value = count_word_occurrences(user_input)
+    max_word_length = max(len(word) for word in word_to_value)
+    for word, count in sorted(word_to_value.items()):
+        print(f"{word:{max_word_length}} : {count}")
+
+def count_word_occurrences(text):
+    words = text.split()
+    word_to_value = {}
+    for word in words:
+        word = word.lower()
+        if word in word_to_value:
+            word_to_value[word] += 1
+        else:
+            word_to_value[word] = 1
+
+    return word_to_value
+
+main()
