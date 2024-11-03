2d7dbda7562bf7f9ab3f004d8cd9be13e90c6a3b
ewabuczkowska <ewakarolina.buczkowska@my.jcu.edu.au>
2024-11-03 16:47:03 +1000

    Update programming language exercise in prac 6

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -3,6 +3,8 @@ CP1404 - Practical 06 - Programming Languages
 Estimate: 1h
 Actual:
 """
+
+
 class ProgrammingLanguage:
 
     def __init__(self, name="", typing="", reflection=False, year=0):
@@ -11,6 +13,10 @@ class ProgrammingLanguage:
         self.typing = typing
         self.reflection = reflection
         self.year = year
+
+    def __str__(self):
+        """Return a string representation of the programming language."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
     def is_dynamic(self):
         """Determine if the language is dynamically typed."""
