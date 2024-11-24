3ce38e3c83f8fdd250f47538244d53686ac505b1
ewabuczkowska <ewakarolina.buczkowska@my.jcu.edu.au>
2024-11-24 21:53:12 +0700

    Add unreliable car class test to prac 9

--- prac_09/car.py
+++ prac_09/car.py
@@ -7,10 +7,7 @@ class Car:
     """Represent a Car object."""
 
     def __init__(self, name="", fuel=0):
-        """Initialise a Car instance.
-
-        fuel: float, one unit of fuel drives one kilometre
-        """
+        """Initialise a Car instance."""
         self.name = name
         self.fuel = fuel
         self._odometer = 0
@@ -24,11 +21,7 @@ class Car:
         self.fuel += amount
 
     def drive(self, distance):
-        """Drive the car a given distance.
-
-        Drive given distance if car has enough fuel
-        or drive until fuel runs out return the distance actually driven.
-        """
+        """Drive the car a given distance."""
         if distance > self.fuel:
             distance = self.fuel
             self.fuel = 0
