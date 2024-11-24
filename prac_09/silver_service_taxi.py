f956ba63f4b1736cb949d5453af03d410d7b7bb0
ewabuczkowska <ewakarolina.buczkowska@my.jcu.edu.au>
2024-11-24 22:43:44 +0700

    Add silver service taxi class and text to prac 9

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,22 @@
+"""
+CP1404  - Practical 09 - 3. Silver Service Taxi
+"""
+
+from prac_09.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    flagfall = 4.50
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(name, fuel)
+        self.fanciness = fanciness
+        self.price_per_km = Taxi.price_per_km * fanciness
+
+    def __str__(self):
+        """Return a string representation of the SilverServiceTaxi."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        """Calculate the current fare."""
+        return self.flagfall + super().get_fare()
