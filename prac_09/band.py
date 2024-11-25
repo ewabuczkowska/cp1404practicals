4e574f660273974b4ddb0c6b8cf9a2153ab3c367
ewabuczkowska <ewakarolina.buczkowska@my.jcu.edu.au>
2024-11-25 08:23:13 +0700

    Add band file to prac 9

---
+++ prac_09/band.py
@@ -0,0 +1,22 @@
+""" CP1404 - Practical 09 - Band class"""
+
+
+class Band:
+    """Band class that contains musicians."""
+
+    def __init__(self, name=""):
+        """Construct a Band with a name and empty musicians collection."""
+        self.name = name
+        self.musicians = []
+
+    def __str__(self):
+        """Return a string representation of a Band."""
+        return f"{self.name} ({', '.join(str(musician) for musician in self.musicians)})"
+
+    def add(self, musician):
+        """Add a musician to the band."""
+        self.musicians.append(musician)
+
+    def play(self):
+        """Return a string showing the musicians playing their first instruments."""
+        return '\n'.join(musician.play() for musician in self.musicians)
