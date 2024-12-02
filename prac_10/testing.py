9a9d1d4984e6f83b43df75ce376777daadb21c68
ewabuczkowska <ewakarolina.buczkowska@my.jcu.edu.au>
2024-12-02 18:31:26 +0700

    Add Todo 4 and 5 to testing file in prac 10

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -53,14 +53,14 @@ def run_tests():
 
 run_tests()
 
-# TODO: 3. Uncomment the following line and run the doctests
+# 3. Uncomment the following line and run the doctests
 # (PyCharm may see your >>> doctest comments and run doctests anyway.)
 doctest.testmod()
 
-# TODO: 4. Fix the failing is_long_word function
+# 4. Fix the failing is_long_word function
 # (Don't change the tests, change the function!)
 
-# TODO: 5. Write and test a function to format a phrase as a sentence,
+# 5. Write and test a function to format a phrase as a sentence,
 # starting with a capital and ending with a single full stop.
 # Important: start with a function header and just use pass as the body
 # then add doctests for 3 tests:
@@ -69,3 +69,19 @@ doctest.testmod()
 # and one more that you decide is a useful test.
 # Run your doctests and watch the tests fail.
 # Then write the body of the function so that the tests pass.
+
+def format_sentence(phrase):
+    """
+    >>> format_sentence('hello')
+    'Hello.'
+    >>> format_sentence('It is an ex parrot.')
+    'It is an ex parrot.'
+    >>> format_sentence('my name is ewa')
+    'My name is ewa.'
+    """
+    if phrase[0].islower():
+        phrase = phrase[0].upper() + phrase[1:]
+    if phrase[-1] != ".":
+        phrase += "."
+    return phrase
+
